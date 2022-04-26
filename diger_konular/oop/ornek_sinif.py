29621050f962dc08cde2517e8629404b397ccde6
ahmtfth1 <fatih005@gmail.com>
2022-04-26 13:59:32 +0300

    diğer konular
    diğer konular yapay zeka vs.

---
+++ diger_konular/oop/ornek_sinif.py
@@ -0,0 +1,15 @@
+# ornek_sinif
+# ornekSinif
+
+class Kalem():
+    # renk = "Mavi"
+    # sınıf değişkeni-özelliği
+    sinif_adi = "Pencil"
+
+    def __init__(self,renk):
+        self.rengi = renk # nesne için değişken-özellik-property tanımlaması
+        print("Yapıcı-constructor metod çalıştı.")
+
+    # metod
+    def yazdir(self,):
+        print("Bu kalemin işi yazı yazmaktır.")
