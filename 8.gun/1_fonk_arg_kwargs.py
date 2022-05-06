87528665d8ecf2eb6c17c0ff04683adbabe87024
ahmtfth1 <fatih005@gmail.com>
2022-05-06 08:41:05 +0300

    yeni yükleme

--- 8.gun/1_fonk_arg_kwargs.py
+++ 8.gun/1_fonk_arg_kwargs.py
@@ -6,20 +6,8 @@
 def toplama(*prt):
     print(prt)
 
-toplama(1,2,3,4,5)
+toplama(5)
 a = 4
 b = 9
 c = 10
 toplama(a,b,c)
-
-
-def fonksiyon2(**kwargs):
-    print(kwargs)
-
-fonksiyon2(dil="python",konu="moduller")
-
-def fonk_3(*args, **kwargs):
-    print(args)
-    print(kwargs)
-
-fonk_3(a,b,c,dil="python",konu="moduller")
